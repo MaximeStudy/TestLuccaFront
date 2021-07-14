069a2d427828600ae0cd5a412644718337f0ce5e
Maxime Study <maxime.study@gmail.com>
2021-07-14 18:27:43 +0200

    finish auth service tests

--- test-lucca-front/src/app/authentification/services/auth.service.spec.ts
+++ test-lucca-front/src/app/authentification/services/auth.service.spec.ts
@@ -51,4 +51,52 @@ describe('AuthService', () => {
     expect(userName).toEqual(expectedUserName);
   });
 
+
+  it('should be authenticated when signup', () => {
+    let isAuth:any;
+    let user: string = "user";
+    let password: string = "pwd";
+
+    service.signUpUser(user,password, password);
+    service.isAuthenticated().subscribe(isAuthenticated => isAuth = isAuthenticated);
+
+    expect(isAuth).toEqual(true);
+  });
+
+  it('should get userName when signup', () => {
+    let expectedUserName:any;
+    let userName: string = "user";
+    let password: string = "pwd";
+
+    service.signUpUser(userName,password, password);
+    service.getUsername().subscribe(userName => expectedUserName = userName);
+
+    expect(userName).toEqual(expectedUserName);
+  });
+
+
+  it('should not be authentified when logout', () => {
+    let isAuth:any;
+    let user: string = "user";
+    let password: string = "pwd";
+
+    service.loginUser(user, password);
+    service.isAuthenticated().subscribe(isAuthenticated => isAuth = isAuthenticated);
+    service.logoutUser();
+
+    expect(isAuth).toEqual(false);
+  });
+
+  it('should empty userName when logout', () => {
+    let expectedUserName:any;
+    let userName: string = "user";
+    let password: string = "pwd";
+
+    service.loginUser(userName,password);
+    service.getUsername().subscribe(userName => expectedUserName = userName);
+    service.logoutUser();
+
+    expect(expectedUserName).toBeNull();
+  });
+
 });
