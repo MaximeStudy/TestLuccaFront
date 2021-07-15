253d84b530e685f0728c80bd3322177a5a592f18
Maxime Study <maxime.study@gmail.com>
2021-07-15 14:16:07 +0200

    add test for local repository

--- test-lucca-front/src/app/chat/services/local-chat-message-repository.service.spec.ts
+++ test-lucca-front/src/app/chat/services/local-chat-message-repository.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { ChatMessage } from '../shared/chat-message';
 
 import { LocalChatMessageRepositoryService } from './local-chat-message-repository.service';
 
@@ -12,5 +13,18 @@ describe('LocalChatMessageRepositoryService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should be created with 0 messages', () => {
+    var currentMessages!:ChatMessage[];
+    service.getMessages().subscribe(messages => currentMessages = messages);
+    expect(currentMessages.length).toEqual(0);
+  });
+
+  it('should contains a message when add', () => {
+    var currentMessages!:ChatMessage[];
+    service.add({content:"hello",sender:"me"});
+    service.getMessages().subscribe(messages => currentMessages = messages);
+    expect(currentMessages.length).toEqual(1);
   });
 });
