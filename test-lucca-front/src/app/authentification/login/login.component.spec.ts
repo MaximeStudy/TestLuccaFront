e828d60ed15e5b8dd2df32844cc93fe36c4423e3
Maxime Study <maxime.study@gmail.com>
2021-07-15 16:57:14 +0200

    check required em tests

--- test-lucca-front/src/app/authentification/login/login.component.spec.ts
+++ test-lucca-front/src/app/authentification/login/login.component.spec.ts
@@ -94,4 +94,61 @@ describe('LoginComponent', () => {
 
     expect(false).toEqual(validate);
   });
+
+  it('should form group have error class when username is not written after selecting input', () => {
+    
+    var userName = fixture.nativeElement.querySelector('#username');
+    var userNameFormGroup = fixture.nativeElement.querySelector('#username-form-group');
+    userName.focus();
+    userName.dispatchEvent(new Event('input'));
+    userName.blur();
+    fixture.detectChanges();
+
+    var hasError = userNameFormGroup.classList.contains("error");
+
+    expect(true).toEqual(hasError);
+  });
+
+  it('should username form group have error with required when input is empty', () => {
+    
+    var userName = fixture.nativeElement.querySelector('#username');
+   
+    userName.focus();
+    userName.dispatchEvent(new Event('input'));
+    userName.blur();
+    fixture.detectChanges();
+
+    var em = fixture.nativeElement.querySelector('#username-form-group em');
+    
+    expect(em.textContent).toEqual("Required");
+  });
+
+  
+  it('should form group have error class when password is not written after selecting input', () => {
+    
+    var password = fixture.nativeElement.querySelector('#password');
+    var passwordFormGroup = fixture.nativeElement.querySelector('#password-form-group');
+    password.focus();
+    password.dispatchEvent(new Event('input'));
+    password.blur();
+    fixture.detectChanges();
+
+    var hasError = passwordFormGroup.classList.contains("error");
+
+    expect(true).toEqual(hasError);
+  });
+
+  it('should password form group have error with required when input is empty', () => {
+    
+    var password = fixture.nativeElement.querySelector('#password');
+   
+    password.focus();
+    password.dispatchEvent(new Event('input'));
+    password.blur();
+    fixture.detectChanges();
+
+    var em = fixture.nativeElement.querySelector('#password-form-group em');
+    
+    expect(em.textContent).toEqual("Required");
+  });
 });
